1138f6ad53bf26457563f410ee73e13d624ee3ff
Young Eric <erikbonopart12@gmail.com>
2025-02-27 20:08:15 +0300

    -setting up confings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import * as path from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+  server: {
+    host: "127.0.0.1",
+    port: 3000,
+  },
+  base: mode === "production" ? "/testTaskKameleoon/" : "/",
+}));
